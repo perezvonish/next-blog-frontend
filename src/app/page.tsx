e652dfb04fdbcf15ee2bb09877a3ea10cd8fdcb3
Vladimir <shokilak@gmail.com>
2023-05-23 20:14:12 +0600

    feat: dev home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,7 @@ import styles from '../styles/page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <h1 style={{color: "black"}}>TEST</h1>
     </main>
   )
 }
