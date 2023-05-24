8b7ca1af5da919350410889bb354db17d97e0970
Vladimir <shokilak@gmail.com>
2023-05-25 00:27:06 +0600

    feat: basic view account page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,9 +1,24 @@
+"use client"
+
 import React from "react";
+import styles from "../../styles/account.module.css"
+import {Button} from "antd";
 
 export default function Home() {
     return (
-        <div>
-            Account/ me
+        <div className={styles.accountView}>
+            <div className={styles.accountViewInfo}>
+                <h1>Username</h1>
+                <h1>first name</h1>
+                <h1>second name</h1>
+            </div>
+
+            <div className={styles.accountViewFunctions}>
+                <h1>Functions</h1>
+                <Button type="primary" danger>
+                    Delete account
+                </Button>
+            </div>
         </div>
     )
 }
