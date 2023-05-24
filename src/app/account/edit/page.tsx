cbaddd5f68836fe235127c54b9e15fac758fcafb
Vladimir <shokilak@gmail.com>
2023-05-25 00:40:20 +0600

    feat: added dev edit account page

--- src/app/account/edit/page.tsx
+++ src/app/account/edit/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import styles from "../../../styles/account.module.css"
 
 export default function Page() {
     return (
-        <div>
-            test
+        <div className={styles.accountEdit}>
+            <h1>
+                Edit account info
+            </h1>
         </div>
     )
 }
